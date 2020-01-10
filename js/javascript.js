2814514608c09fb5ba96d5a5b1689349bc0d6bf5
Ben Lusted <blustedaudio@gmail.com>
2020-01-09 19:02:32 -0800

    Functionality changes
    - Added sign up and log in containers.
        - sign up will show up when you arrive at the site
    - Stored user input when setting user and password

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+window.localStorage.clear();
+
 if(!localStorage.getItem('user')) {
   window.localStorage.setItem('user','username');
 }
@@ -8,6 +10,14 @@ if(!localStorage.getItem('pass')) {
 window.addEventListener('load',function() {
   //Check if user and pass have been set. If not,
   //Bring up 'Sign up' page first.
+
+  if(localStorage.getItem('user') === 'username' && localStorage.getItem('pass') === 'password')
+  {
+    document.getElementById('signCont').style.display = 'block';
+  }
+  else {
+    document.getElementById('logCont').style.display = 'block';
+  }
 
   //Once signed up, save info in localStorage
   //Then run the 'successful' stuff.
@@ -24,8 +34,38 @@ window.addEventListener('load',function() {
   else {
     console.log("Found username in storage");
   }
+
+  if(pass === 'password') {
+    console.log("Need to set a password");
+  } else {
+    console.log("Found password in storage");
+  }
 });
 
 function validate_info() {
+  var this_user = localStorage.getItem('user');
+  console.log(this_user);
+}
 
+function signup() {
+  var new_user;
+  var new_pass;
+  if(document.getElementById("i-user").value != "") {
+    new_user = document.getElementById("i-user").value;
+    window.localStorage.setItem('user',new_user);
+    console.log(new_user);
+  } else {
+    console.log("No username recorded");
+  }
+  if(document.getElementById("i-pass").value != "") {
+    new_pass = document.getElementById("i-pass").value;
+    window.localStorage.setItem('pass',new_pass);
+    console.log(new_pass);
+  } else {
+    console.log("No password recorded");
+  }
+
+  //Password should be 8+ characters long
+  //Check if password contains an uppercase letter and special character ($,!,%)
+  //Username can be whatever
 }
